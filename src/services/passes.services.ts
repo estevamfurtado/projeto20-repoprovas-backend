43ccc1382ac2fe42cd2c5f3e4c8c77e14099de76
Estevam <estevamfurtado@gmail.com>
2022-07-14 09:44:43 -0300

    refact: passes services

--- src/services/passes.services.ts
+++ src/services/passes.services.ts
@@ -3,93 +3,111 @@ import { client } from "../database";
 import { AppError } from '../utils/errors/AppError';
 import {crypt} from '../utils/crypt';
 import { chalkLogger } from '../utils/chalkLogger';
-import { UserInsertData } from '../models/types/users.types';
-import { Card, NewCard } from '../models/types/cards.types';
 import { validateJoiSchemaFromObjectOrCrash } from '../utils/joiUtils';
 import { Passes } from '@prisma/client';
+import Joi from 'joi';
 
-export function getNewPassSchemaOrCrash (type: string) {
-    chalkLogger.log('service', 'Getting schema');
-    const passSchemas = joiSchemas.Passes.new;
-    const passSchema = passSchemas[type as keyof typeof passSchemas] ?? undefined;
-    if (!passSchema) {
-        throw new AppError(400, `Invalid type: ${type}`);
+
+export interface TypeUtil {
+    type: string;
+    cryptedColumns: string[];
+    outputColumns: string[];
+    joi: {
+        new: Joi.AnySchema;
+        update: Joi.AnySchema;
     }
-    return passSchema;
 }
 
-export function getUpdatePassSchemaOrCrash (type: string) {
-    chalkLogger.log('service', 'Getting schema');
-    const passSchemas = joiSchemas.Passes.update;
-    const passSchema = passSchemas[type as keyof typeof passSchemas] ?? undefined;
-    if (!passSchema) {
-        throw new AppError(400, `Invalid type: ${type}`);
+const typesUtils: TypeUtil[] = [
+    {   type: 'card', 
+        cryptedColumns: ['cvv', 'password'],
+        outputColumns: ['id', 'createdAt', 'title', 'name', 'number', 'expiry', 'cardType', 'cvv', 'password'],
+        joi: {new: joiSchemas.Passes.new.card, update: joiSchemas.Passes.update.card}},
+    {   type: 'wifi',
+        cryptedColumns: ['password'],
+        outputColumns: ['id', 'createdAt', 'title', 'network', 'password'],
+        joi: {new: joiSchemas.Passes.new.wifi, update: joiSchemas.Passes.update.wifi}},
+    {   type: 'credential',
+        cryptedColumns: ['password'],
+        outputColumns: ['id', 'createdAt', 'title', 'login', 'password'],
+        joi: {new: joiSchemas.Passes.new.credential, update: joiSchemas.Passes.update.credential}},
+    {   type: 'note',
+        cryptedColumns: [],
+        outputColumns: ['id', 'createdAt', 'title', 'content'],
+        joi: {new: joiSchemas.Passes.new.note, update: joiSchemas.Passes.update.note}}
+];
+
+function getTypeUtilOrCrash (type: string) {
+    // if there is not, crash
+    const typeUtil = typesUtils.find((typeUtil) => typeUtil.type === type);
+    if (!typeUtil) {
+        throw new AppError(404, `Invalid pass type ${type}`);
     }
-    return passSchema;
+    return typeUtil;
 }
 
-function cryptPass(data: any) {
-    if (data.password) {
-        data.password = crypt.cryptr.encrypt(data.password);
-    }
-    if (data.cvv) {
-        data.cvv = crypt.cryptr.encrypt(data.cvv);
+function cryptKeys(data: any, keys: string[]) {    
+    keys.forEach((key) => {
+        if (data[key]) {
+            data[key] = crypt.cryptr.encrypt(data[key]);
+        }
+    });
+    return data;
+}
+
+function decryptKeys(data: any, keys: string[]) {
+    keys.forEach((key) => {
+        if (data[key]) {
+            data[key] = crypt.cryptr.decrypt(data[key]);
+        }
+    });
+    return data;
+}
+
+function validateData(data: any, joiSchema: Joi.AnySchema) {
+    const {error} = joiSchema.validate(data);
+    if (error) {
+        throw new AppError(400, error.message);
     }
     return data;
 }
 
-function decryptPass(data: any) {
-    if (data.password) {
-        data.password = crypt.cryptr.decrypt(data.password);
-    }
-    if (data.cvv) {
-        data.cvv = crypt.cryptr.decrypt(data.cvv);
-    }
-    return data;
-}
-
-const formattedPassColumns = {
-    card: ['id', 'createdAt', 'title', 'name', 'number', 'expiry', 'cardType', 'cvv', 'password'],
-    wifi: ['id', 'createdAt', 'title', 'network', 'password'],
-    credential: ['id', 'createdAt', 'title', 'url', 'login', 'password'],
-    note: ['id', 'createdAt', 'title', 'content'],
-}
-
-function formatPass(data: Passes) {
-    const {type} = data;
-    const columns = formattedPassColumns[type as keyof typeof formattedPassColumns];
+function formatPass(data: Passes, typeUtil: TypeUtil) {
+    const columns = typeUtil.outputColumns;
     const formattedData = columns.reduce((acc, column) => {
         acc[column] = data[column as keyof typeof data];
         return acc;
     }
     , {} as any);
-    return decryptPass(formattedData);
+    return decryptKeys(formattedData, typeUtil.cryptedColumns);
 }
 
-function clearData(data: any) {
+function clearData(data: any, typeUtil: TypeUtil) {
     const clear = Object.keys(data).forEach((key) => {
         if (data[key] === null || data[key] === undefined) {
             delete data[key];
         }
     });
     if (data.passId) {delete data.passId}
-    return cryptPass(data);
+    return decryptKeys(data, typeUtil.cryptedColumns);
 }
 
-function formatPassesObject(passes: any) {
+function formatPassesObject(passes: any, typeUtil: TypeUtil) {
     const array = Object.values(passes) as Passes[];
-    return array.map((pass) => {return formatPass(pass)});
+    return array.map((pass) => {return formatPass(pass, typeUtil)});
 }
+
+// -------------------------
 
 export async function insert (data: any) {
-    const insertData = validateInsertData(data) as any;
-    const cryptedData = cryptPass(insertData);
+    const typeUtil = getTypeUtilOrCrash(data.type);
+    const dataToInsert = validateData(data, typeUtil.joi.new);
+    const cryptedData = cryptKeys(dataToInsert, typeUtil.cryptedColumns);
     const pass = await client.passes.create({data: cryptedData});
-    return formatPass(pass);
+    return formatPass(pass, typeUtil);
 }
 
 export async function findById(passId: number, userId: number) {
-    console.log(passId, userId)
     const pass = await client.passes.findFirst({
         where: {
             id: {
@@ -102,11 +120,11 @@ export async function findById(passId: number, userId: number) {
         throw new AppError(404, 'Pass not found');
     }
 
-    return formatPass(pass);
+    const typeUtil = getTypeUtilOrCrash(pass.type);
+    return formatPass(pass, typeUtil);
 }
 
 export async function deleteById(passId: number, userId: number) {
-
     await findById(passId, userId);
     const result = await client.passes.delete({
         where: {
@@ -117,16 +135,18 @@ export async function deleteById(passId: number, userId: number) {
 }
 
 export async function updateById (passId: number, userId:number, data: any) {
-    // find type of pass by id
     const pass = await client.passes.findFirst({where: { id: {equals: passId}},})
-    const type = pass?.type || null;
-    const updateData = validateUpdateData({...data, type}) as any;
-    const clearedData = clearData(updateData);
+    if (!pass || pass.userId !== userId) {
+        throw new AppError(404, 'Pass not found');
+    }
+    const typeUtil = getTypeUtilOrCrash(pass.type);
+    const dataToUpdate = validateData(data, typeUtil.joi.update);
+    const clearedData = clearData(dataToUpdate, typeUtil);
     const updatedPass = await client.passes.update({data: clearedData, where: {id: passId}});
-    return formatPass(updatedPass);
+    return formatPass(updatedPass, typeUtil);
 }
 
-export async function getAll (userId: number) {
+export async function getTypesCounter (userId: number) {
     const typesPassesCounter = await client.passes.groupBy({
         by: ['type'],
         where: {
@@ -138,21 +158,30 @@ export async function getAll (userId: number) {
             _all: true,
         },
     })
-    return formatTypesPassesCounter(typesPassesCounter);
+    return formatTypesCounter(typesPassesCounter);
 }
 
-function formatTypesPassesCounter(typesPassesCounter: any){
-    const types = {wifi: 0, card: 0, credential: 0, note: 0};
+function formatTypesCounter(typesPassesCounter: any){
+    const counter = typesUtils.map((typeUtil) => {
+        const type = typeUtil.type;
+        const count = 0;
+        return {type, count};
+    });
     const keys = Object.keys(typesPassesCounter);
     keys.forEach((key) => {
         const type = typesPassesCounter[key].type;
         const value = typesPassesCounter[key]._count._all;
-        types[type as keyof typeof types] = value;
+        counter.forEach((c) => {
+            if (c.type === type) {
+                c.count = value;
+            }
+        });
     });
-    return types;
+    return counter;
 }
 
-export async function getByType (userId: number, type: string) {
+export async function findByType (userId: number, type: string) {
+    const typeUtil = getTypeUtilOrCrash(type);
     const passes = await client.passes.findMany({
         where: {
             userId: {
@@ -166,22 +195,5 @@ export async function getByType (userId: number, type: string) {
             createdAt: 'desc',
         },
     })
-    return formatPassesObject(passes);
-}
-
-
-export function validateInsertData (insertData: any) {
-    chalkLogger.log('service', 'Validating insert data');
-    const { type } = insertData;
-    const joiSchema = getNewPassSchemaOrCrash(type as string);
-    const object = validateJoiSchemaFromObjectOrCrash(insertData, joiSchema);
-    return object;
-}
-
-export function validateUpdateData (insertData: any) {
-    chalkLogger.log('service', 'Validating insert data');
-    const { type } = insertData;
-    const joiSchema = getUpdatePassSchemaOrCrash(type as string);
-    const object = validateJoiSchemaFromObjectOrCrash(insertData, joiSchema);
-    return object;
+    return formatPassesObject(passes, typeUtil);
 }
