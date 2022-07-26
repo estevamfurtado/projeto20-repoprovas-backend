151ca302eafa306760d5238f9815b120d9d6725a
Estevam <estevamfurtado@gmail.com>
2022-07-26 13:20:15 -0300

    fix: build

--- dist/utils/sqlUtils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.mapObjectToUpdateQuery = void 0;
-function mapObjectToUpdateQuery({ object, offset = 1 }) {
-    const objectColumns = Object.keys(object).map((key, index) => `"${key}"=$${index + offset}`).join(",");
-    const objectValues = Object.values(object);
-    return { objectColumns, objectValues };
-}
-exports.mapObjectToUpdateQuery = mapObjectToUpdateQuery;
